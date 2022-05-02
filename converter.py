1ce93348b2e40bea8b0e043ae947df704dd122cc
kalyanrohan <sri.rohan@binus.ac.id>
2022-05-02 20:41:54 +0700

    updated dataset

--- converter.py
+++
@@ -1,6 +0,0 @@
-import moviepy.editor as moviepy
-from PyQt5.QtWidgets import QFileDialog
-file_name = QFileDialog.getOpenFileName()
-print(file_name)
-clip=moviepy.VideoFileClip(file_name)
-new_clip=clip.write_videofile("myvideo.mp4")
